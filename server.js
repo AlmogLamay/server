2391284117ad8ace2197ce0f947e9815a10cca09
AlmogLamay <almoglamay10@gmail.com>
2023-01-29 20:59:24 +0200

    server2

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ const server = http.createServer(app);
 //client
 const io = new Server(server, {
 	cors: {
-		origin: 'http://localhost:3000',
+		//origin: 'http://localhost:3000',
+		origin: 'https://client-new.onrender.com',
 		methods: ['GET', 'POST'],
 	},
 });
